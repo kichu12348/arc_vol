f7d245a9bbf8086760367af4a10bc420332546b7
Mahadevan Reji <rmahadevan574@gmail.com>
2024-10-31 23:15:02 +0530

    fix: correct case of User model import

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Create a new user
 router.post('/', async (req, res) => {
